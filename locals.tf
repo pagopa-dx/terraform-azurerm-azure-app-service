3687129f4e49e7d67a5fb9cb2d19185722b72805
christian-calabrese <christian.calabrese@pagopa.it>
2024-10-07 10:45:19 +0200

    Initial commit for module azure_app_service

---
+++ locals.tf
@@ -0,0 +1,34 @@
+locals {
+  location_short = var.environment.location == "italynorth" ? "itn" : var.environment.location == "westeurope" ? "weu" : var.environment.location == "germanywestcentral" ? "gwc" : "neu"
+  project        = "${var.environment.prefix}-${var.environment.env_short}-${local.location_short}"
+
+  subnet = {
+    enable_service_endpoints = var.subnet_service_endpoints != null ? concat(
+      var.subnet_service_endpoints.cosmos ? ["Microsoft.CosmosDB"] : [],
+      var.subnet_service_endpoints.web ? ["Microsoft.Web"] : [],
+      var.subnet_service_endpoints.storage ? ["Microsoft.Storage"] : [],
+    ) : []
+  }
+
+  app_service_plan = {
+    enable = var.app_service_plan_id == null
+  }
+
+  app_service = {
+    sku_name               = local.sku_name_mapping[local.tier]
+    zone_balancing_enabled = local.tier != "s"
+    is_slot_enabled        = local.tier == "s" ? 0 : 1
+  }
+
+  app_service_slot = {
+    name = "staging"
+  }
+
+  application_insights = {
+    enable = var.application_insights_connection_string != null
+  }
+
+  private_dns_zone = {
+    resource_group_name = var.private_dns_zone_resource_group_name == null ? var.virtual_network.resource_group_name : var.private_dns_zone_resource_group_name
+  }
+}
